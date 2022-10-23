8f008ed0a4393575af1fdbb0cb82b6b08ef7cfb0
UpToCase <99139170+UpToCase@users.noreply.github.com>
2022-10-23 17:48:09 +0200

    add @Attachment to the method allureScreenshot

--- src/test/java/utils/AllureUtils.java
+++ src/test/java/utils/AllureUtils.java
@@ -1,11 +1,13 @@
 package utils;
 
+import io.qameta.allure.Attachment;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 
 public class AllureUtils {
 
+    @Attachment(value = "screenshots", type = "image/png")
     public static byte[] allureScreenshots(WebDriver driver){
         return((TakesScreenshot)driver).getScreenshotAs(OutputType.BYTES);
     }
