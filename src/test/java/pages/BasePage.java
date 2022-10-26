2ccb71a12972c83c63eb4114c5bc1158f8da19c9
UpToCase <99139170+UpToCase@users.noreply.github.com>
2022-10-26 19:59:44 +0200

    Added option to disregard punctuation and punctuation when checking account and contact

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -33,8 +33,9 @@ public abstract class BasePage {
 
     public BasePage validateNewAccountText(String label, String expectedText) {
         String locator = "//span[text()='%s']//ancestor::records-record-layout-item//*[@slot='outputField']";
-        assertEquals(driver.findElement(By.xpath(String.format(locator, label))).getText(),
-                expectedText, "Wrong text is displaced");
+        assertEquals(driver.findElement(By.xpath(String.format(locator, label))).getText().replaceAll("[\\pP\\s]",
+                ""), expectedText.replaceAll("[\\pP\\s]", ""),
+                "Wrong text is displaced");
         return this;
     }
 
@@ -42,15 +43,15 @@ public abstract class BasePage {
         String locator = "//span[text()='%s']//ancestor::records-record-layout-item//a";
         assertEquals(driver.findElement(By.xpath(String.format(locator, label)))
                         .getText().replaceAll("[\\pP\\s]", ""),
-                expectedAddressInfo, "Wrong text is displaced");
+                expectedAddressInfo.replaceAll("[\\pP\\s]", ""), "Wrong text is displaced");
         return this;
     }
 
     public BasePage validateNewContactLinks(String label, String organisationCorrectLocator, String expectedLink) {
         String linkLocator = "//span[(text()='%s')]//ancestor::*[contains(@class,'slds-form__item')]//%s";
         assertEquals(driver.findElement(By.xpath(String.format(linkLocator, label, organisationCorrectLocator)))
-                        .getText().replaceAll("\n| |,", ""),
-                expectedLink, "Wrong link is displaced");
+                        .getText().replaceAll("[\\pP\\s]", ""),
+                expectedLink.replaceAll("[\\pP\\s]", ""), "Wrong link is displaced");
         return this;
     }
 
@@ -58,8 +59,8 @@ public abstract class BasePage {
         String locatorInfo = "//span[(text()='%s')]//ancestor::*[contains(@class,'slds-form-element')]" +
                 "//*[@slot='outputField']";
         assertEquals(driver.findElement(By.xpath(String.format(locatorInfo, label))).getText().
-                        replaceAll(" ", ""), expectedContactInfo,
-                "Wrong information is displaced");
+                        replaceAll("[\\pP\\s]", ""), expectedContactInfo.replaceAll("[\\pP\\s]",
+                ""), "Wrong information is displaced");
         return this;
     }
 
